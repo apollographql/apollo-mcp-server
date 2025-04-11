002700e1c6f3a1c1dc87b58b28f8c4849c3451d2
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2025-04-11 11:55:21 -0600

    parse name from operation

--- crates/mcp-apollo-server/src/json_schema_to_toolbox.rs
+++ crates/mcp-apollo-server/src/json_schema_to_toolbox.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use rmcp::{handler::server::tool::{ToolBox, ToolBoxItem, ToolCallContext}, model::{object, CallToolResult, ErrorData, Tool}, schemars::schema::RootSchema, serde_json::json, ServerHandler};
+use rmcp::{handler::server::tool::{ToolBox, ToolBoxItem, ToolCallContext}, model::{object, CallToolResult, ErrorData, Tool}, serde_json::json, ServerHandler};
+
+use crate::operation_to_json_schema::ToolDefinition;
 
 #[derive(Debug, Clone, Default)]
 pub struct Server {}
@@ -24,10 +26,19 @@ impl Server {
 }
 
 
-pub fn json_schema_to_toolbox(schema: RootSchema, mut tool_box: ToolBox<Server>) {
-    let tool = Tool::new("name", "description", Arc::new(object(json!(schema))));
-    let _toolbox_item = ToolBoxItem::new(tool, |_tool_call_context: ToolCallContext<'_, Server>| {
-        Box::pin(async { Err(rmcp::model::ErrorData::new(rmcp::model::ErrorCode::RESOURCE_NOT_FOUND, "An error occurred", None)) })
-    });
-    tool_box.add(_toolbox_item);
+pub fn json_schema_to_toolbox(definition: ToolDefinition, mut tool_box: ToolBox<Server>) {
+    let tool = Tool::new(
+        definition.name,
+        definition.description,
+        // TODO: validate this works? 
+        Arc::new(object(json!(definition.schema)))
+    );
+
+    let toolbox_item = ToolBoxItem::new(
+        tool, 
+        |_tool_call_context: ToolCallContext<'_, Server>| {
+            Box::pin(async { Err(rmcp::model::ErrorData::new(rmcp::model::ErrorCode::RESOURCE_NOT_FOUND, "An error occurred", None)) })
+        }
+    );
+    tool_box.add(toolbox_item);
 }
