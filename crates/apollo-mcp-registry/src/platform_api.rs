8adc3b1e10c781fb50e894fb13add57ce2963623
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2025-06-11 17:27:34 -0700

    allow overriding endpoints like rover and router

--- crates/apollo-mcp-registry/src/platform_api.rs
+++ crates/apollo-mcp-registry/src/platform_api.rs
@@ -1,11 +1,14 @@
 use secrecy::SecretString;
 use std::fmt::Debug;
 use std::time::Duration;
+use url::Url;
 
 pub mod operation_collections;
 
+const DEFAULT_PLATFORM_API: &str = "https://registry.apollographql.com/api/graphql";
+
 /// Configuration for polling Apollo Uplink.
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 pub struct PlatformApiConfig {
     /// The Apollo key: `<YOUR_GRAPH_API_KEY>`
     pub apollo_key: SecretString,
@@ -15,4 +18,26 @@ pub struct PlatformApiConfig {
 
     /// The HTTP client timeout for each poll
     pub timeout: Duration,
+
+    /// The URL of the Apollo registry
+    pub registry_url: Url,
+}
+
+impl PlatformApiConfig {
+    /// Creates a new `PlatformApiConfig` with the given Apollo key and default values for other fields.
+    pub fn new(
+        apollo_key: SecretString,
+        poll_interval: Duration,
+        timeout: Duration,
+        registry_url: Option<Url>,
+    ) -> Self {
+        Self {
+            apollo_key,
+            poll_interval,
+            timeout,
+            #[allow(clippy::expect_used)]
+            registry_url: registry_url
+                .unwrap_or(Url::parse(DEFAULT_PLATFORM_API).expect("default URL should be valid")),
+        }
+    }
 }
