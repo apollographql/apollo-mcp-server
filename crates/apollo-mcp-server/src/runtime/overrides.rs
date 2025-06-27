fdbf68c1be18c4cd44c222a03e84ae6b4ea990bc
Nicholas Cioli <nicholas.cioli@apollographql.com>
2025-06-27 14:18:51 -0400

    address comments

--- crates/apollo-mcp-server/src/runtime/overrides.rs
+++ crates/apollo-mcp-server/src/runtime/overrides.rs
@@ -4,15 +4,13 @@ use serde::Deserialize;
 
 /// Overridable flags
 #[derive(Debug, Deserialize, Default, JsonSchema)]
+#[serde(default)]
 pub struct Overrides {
     /// Disable type descriptions to save on context-window space
     pub disable_type_description: bool,
 
     /// Disable schema descriptions to save on context-window space
     pub disable_schema_description: bool,
-
-    /// Expose the schema to the MCP client through `introspect` and `execute` tools
-    pub enable_introspection: bool,
 
     /// Expose a tool that returns the URL to open a GraphQL operation in Apollo Explorer (requires APOLLO_GRAPH_REF)
     pub enable_explorer: bool,
