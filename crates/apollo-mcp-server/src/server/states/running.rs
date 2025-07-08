16bd7294fde005869316d4f24e52eb74b1f00e15
Matthew Hawkins <matthew.hawkins@apollographql.com>
2025-07-08 14:25:30 -0600

    Fix formatting

--- crates/apollo-mcp-server/src/server/states/running.rs
+++ crates/apollo-mcp-server/src/server/states/running.rs
@@ -59,16 +59,18 @@ impl Running {
             .cloned()
             .map(|operation| operation.into_inner())
             .filter_map(|operation| {
-                operation.into_operation(
-                    &schema,
-                    self.custom_scalar_map.as_ref(),
-                    self.mutation_mode,
-                    self.disable_type_description,
-                    self.disable_schema_description,
-                ).unwrap_or_else(|error| {
-                    error!("Invalid operation: {}", error);
-                    None
-                })
+                operation
+                    .into_operation(
+                        &schema,
+                        self.custom_scalar_map.as_ref(),
+                        self.mutation_mode,
+                        self.disable_type_description,
+                        self.disable_schema_description,
+                    )
+                    .unwrap_or_else(|error| {
+                        error!("Invalid operation: {}", error);
+                        None
+                    })
             })
             .collect();
 
@@ -99,16 +101,18 @@ impl Running {
             let updated_operations: Vec<Operation> = operations
                 .into_iter()
                 .filter_map(|operation| {
-                    operation.into_operation(
-                        schema,
-                        self.custom_scalar_map.as_ref(),
-                        self.mutation_mode,
-                        self.disable_type_description,
-                        self.disable_schema_description,
-                    ).unwrap_or_else(|error| {
-                        error!("Invalid operation: {}", error);
-                        None
-                    })
+                    operation
+                        .into_operation(
+                            schema,
+                            self.custom_scalar_map.as_ref(),
+                            self.mutation_mode,
+                            self.disable_type_description,
+                            self.disable_schema_description,
+                        )
+                        .unwrap_or_else(|error| {
+                            error!("Invalid operation: {}", error);
+                            None
+                        })
                 })
                 .collect();
 
