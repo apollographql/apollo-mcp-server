461088e7960945e4cb3c1b2e8628ad7642c209fc
Armando Locay <armando.locay@apollographql.com>
2025-07-14 12:15:43 -0400

    feat: adding apollo-mcp-proxy crate and adding a set of proxy config values for it

--- crates/apollo-mcp-server/src/main.rs
+++ crates/apollo-mcp-server/src/main.rs
@@ -14,6 +14,7 @@ use clap::builder::styling::{AnsiColor, Effects};
 use runtime::IdOrDefault;
 use tracing::{Level, info, warn};
 use tracing_subscriber::EnvFilter;
+use apollo_mcp_proxy::client::start_proxy_client;
 
 mod runtime;
 
@@ -131,8 +132,8 @@ async fn main() -> anyhow::Result<()> {
         .then(|| config.graphos.graph_ref())
         .transpose()?;
 
-    Ok(Server::builder()
-        .transport(config.transport)
+    let mcp_server = Server::builder()
+        .transport(config.transport.clone())
         .schema_source(schema_source)
         .operation_source(operation_source)
         .endpoint(config.endpoint.into_inner())
@@ -157,6 +158,24 @@ async fn main() -> anyhow::Result<()> {
         .index_memory_bytes(config.introspection.search.index_memory_bytes)
         .health_check(config.health_check)
         .build()
-        .start()
-        .await?)
+        .start();
+
+    match config.transport {
+        Transport::StreamableHttp { proxy, proxy_endpoint, .. } => {
+            if proxy {
+                let mut endpoint = proxy_endpoint;
+                if !endpoint.starts_with("http") {
+                    endpoint = format!("http://{endpoint}");
+                }
+                
+                let proxy_client = start_proxy_client(endpoint.as_str());
+                let (_, _ ) = tokio::join!(mcp_server, proxy_client);
+            } else {
+                mcp_server.await?;
+            }
+        }
+        _ => { mcp_server.await?; }
+    }
+
+    Ok(())
 }
