8b1b584241fa7005d68476e0ff1005a57d806e5b
Nicholas Cioli <nicholas.cioli@apollographql.com>
2025-07-31 15:49:23 -0400

    add tests

--- crates/apollo-mcp-server/src/auth.rs
+++ crates/apollo-mcp-server/src/auth.rs
@@ -11,20 +11,20 @@ use axum_extra::{
     headers::{Authorization, authorization::Bearer},
 };
 use http::Method;
-use jsonwebtoken::{Algorithm, Validation, decode, decode_header};
-use jwks::Jwks;
+use networked_token_validator::NetworkedTokenValidator;
 use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use tower_http::cors::{Any, CorsLayer};
-use tracing::{info, warn};
 use url::Url;
 
+mod networked_token_validator;
 mod protected_resource;
 mod valid_token;
 mod www_authenticate;
 
 use protected_resource::ProtectedResource;
 pub(crate) use valid_token::ValidToken;
+use valid_token::ValidateToken;
 use www_authenticate::WwwAuthenticate;
 
 /// Auth configuration options
@@ -86,93 +86,31 @@ async fn oauth_validate(
     mut request: Request,
     next: Next,
 ) -> Result<Response, (StatusCode, TypedHeader<WwwAuthenticate>)> {
-    let resource_url = {
+    // Consolidated unauthorized error for use with any fallible step in this process
+    let unauthorized_error = || {
         let mut resource = auth_config.resource.clone();
         resource.set_path("/.well-known/oauth-protected-resource");
 
-        resource
-    };
-
-    match token {
-        Some(bearer) if token_is_valid(&auth_config, bearer.token()).await => {
-            // Insert new context to ensure that handlers only use our enforced token verification
-            // for propagation
-            request.extensions_mut().insert(ValidToken(bearer.0));
-
-            let response = next.run(request).await;
-            Ok(response)
-        }
-
-        _ => Err((
+        (
             StatusCode::UNAUTHORIZED,
             TypedHeader(WwwAuthenticate::Bearer {
-                resource_metadata: resource_url,
+                resource_metadata: resource,
             }),
-        )),
-    }
-}
-
-/// Ensure that the supplied token is valid for the given config
-async fn token_is_valid(auth_config: &Config, jwt: &str) -> bool {
-    #[derive(Clone, Debug, Serialize, Deserialize)]
-    pub struct Claims {
-        pub aud: String,
-        pub sub: String,
-    }
-
-    let Ok(header) = decode_header(jwt) else {
-        return false;
-    };
-    let Some(ref key_id) = header.kid else {
-        return false;
+        )
     };
 
-    for server in &auth_config.servers {
-        let Ok(jwks) =
-            Jwks::from_oidc_url(format!("{server}/.well-known/oauth-authorization-server"))
-                .await
-                .inspect_err(|e| {
-                    warn!("could not fetch OIDC information from {server}: {e}. Skipping...");
-                })
-        else {
-            continue;
-        };
+    let validator = NetworkedTokenValidator::new(&auth_config.audiences, &auth_config.servers);
+    let token = token.ok_or_else(unauthorized_error)?;
 
-        let Some(jwk) = jwks.keys.get(key_id) else {
-            continue;
-        };
+    let valid_token = validator
+        .validate(token.0)
+        .await
+        .ok_or_else(unauthorized_error)?;
 
-        let validation = {
-            let mut val = Validation::new(match jwk.alg {
-                jsonwebtoken::jwk::KeyAlgorithm::HS256 => Algorithm::HS256,
-                jsonwebtoken::jwk::KeyAlgorithm::HS384 => Algorithm::HS384,
-                jsonwebtoken::jwk::KeyAlgorithm::HS512 => Algorithm::HS512,
-                jsonwebtoken::jwk::KeyAlgorithm::ES256 => Algorithm::ES256,
-                jsonwebtoken::jwk::KeyAlgorithm::ES384 => Algorithm::ES384,
-                jsonwebtoken::jwk::KeyAlgorithm::RS256 => Algorithm::RS256,
-                jsonwebtoken::jwk::KeyAlgorithm::RS384 => Algorithm::RS384,
-                jsonwebtoken::jwk::KeyAlgorithm::RS512 => Algorithm::RS512,
-                jsonwebtoken::jwk::KeyAlgorithm::PS256 => Algorithm::PS256,
-                jsonwebtoken::jwk::KeyAlgorithm::PS384 => Algorithm::PS384,
-                jsonwebtoken::jwk::KeyAlgorithm::PS512 => Algorithm::PS512,
-                jsonwebtoken::jwk::KeyAlgorithm::EdDSA => Algorithm::EdDSA,
-                jsonwebtoken::jwk::KeyAlgorithm::RSA1_5 => todo!(),
-                jsonwebtoken::jwk::KeyAlgorithm::RSA_OAEP => todo!(),
-                jsonwebtoken::jwk::KeyAlgorithm::RSA_OAEP_256 => todo!(),
-            });
-            val.set_audience(&auth_config.audiences);
+    // Insert new context to ensure that handlers only use our enforced token verification
+    // for propagation
+    request.extensions_mut().insert(valid_token);
 
-            val
-        };
-        match decode::<Claims>(jwt, &jwk.decoding_key, &validation) {
-            Ok(claims) => {
-                info!("Token passed validation with claims: {claims:?}");
-                return true;
-            }
-            Err(e) => warn!("Token failed validation with error: {e}"),
-        };
-    }
-
-    info!("Token did not pass validation");
-    false
+    let response = next.run(request).await;
+    Ok(response)
 }
