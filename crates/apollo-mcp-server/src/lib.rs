7e3953ba2b6631587ad6b51c8fe4fc22edaae425
Dale Seo <dale.seo@apollographql.com>
2025-11-26 14:10:36 -0500

    chore: clean up sanitize module

--- crates/apollo-mcp-server/src/lib.rs
+++ crates/apollo-mcp-server/src/lib.rs
@@ -1,23 +1,22 @@
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
-pub mod apps;
-pub mod auth;
+pub(crate) mod apps;
+pub(crate) mod auth;
 pub mod cors;
 pub mod custom_scalar_map;
 pub mod errors;
-pub mod event;
+pub(crate) mod event;
 mod explorer;
 mod graphql;
 pub mod headers;
 pub mod health;
 mod introspection;
-pub mod json_schema;
+pub(crate) mod json_schema;
 pub(crate) mod meter;
 pub mod operations;
-pub mod sanitize;
 pub(crate) mod schema_tree_shake;
 pub mod server;
-pub mod telemetry_attributes;
+pub(crate) mod telemetry_attributes;
 
 /// These values are generated at build time by build.rs using telemetry.toml as input.
 pub mod generated {
